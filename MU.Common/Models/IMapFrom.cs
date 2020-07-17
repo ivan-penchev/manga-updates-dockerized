407809bc8cd8f96280c13f5af09cf1b7cbccd386
Ivan Penchev <thefolenangel@gmail.com>
2020-07-17 09:24:01 +0200

    added automapping profile models for the infrastructure:

---
+++ MU.Common/Models/IMapFrom.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MU.Common.Models
+{
+    public interface IMapFrom<T>
+    {
+        void Mapping(Profile mapper) => mapper.CreateMap(typeof(T), this.GetType());
+    }
+}
