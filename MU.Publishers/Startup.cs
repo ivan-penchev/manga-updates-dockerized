5fa203c6f0a4817de356bf0fbb4e6e3f942a063e
Ivan Penchev <ivanpenchev@ue-varna.bg>
2020-07-18 12:36:06 +0200

    Added Publishers database

--- MU.Publishers/Startup.cs
+++ MU.Publishers/Startup.cs
@@ -8,7 +8,9 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using MU.Common.Infrastructure;
+using MU.Common.Services;
+using MU.Publishers.Data;
 
 namespace MU.Publishers
 {
@@ -24,25 +26,17 @@ namespace MU.Publishers
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services
+                .AddWebMicroService<PublishersDbContext>(this.Configuration)
+                .AddTransient<IDataSeeder, PublisherDbSeeder>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
+            app
+                .UseWebMicroService(env)
+                .Initialize();
         }
     }
 }
